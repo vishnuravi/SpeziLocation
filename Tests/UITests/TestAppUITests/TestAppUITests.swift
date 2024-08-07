5139cdca19e2abd75722e00484b1e387266c0944
Vishnu Ravi <vishnur@stanford.edu>
2024-08-07 13:55:59 -0400

    Delete app before each test

--- Tests/UITests/TestAppUITests/TestAppUITests.swift
+++ Tests/UITests/TestAppUITests/TestAppUITests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+import XCTestExtensions
 
 
 class TestAppUITests: XCTestCase {
@@ -14,6 +15,14 @@ class TestAppUITests: XCTestCase {
         try super.setUpWithError()
         
         continueAfterFailure = false
+        
+        let app = XCUIApplication()
+        
+        #if !os(macOS)
+        app.deleteAndLaunch(withSpringboardAppName: "TestApp")
+        #else
+        app.launch()
+        #endif
     }
     
     func testRequestPermissions() throws {
@@ -26,8 +35,8 @@ class TestAppUITests: XCTestCase {
         app.buttons["Request When In Use Permission"].tap()
         
         let springboard = XCUIApplication(bundleIdentifier: "com.apple.springboard")
-        if springboard.buttons["Allow Once"].waitForExistence(timeout: 10) {
-            springboard.buttons["Allow Once"].tap()
+        if springboard.buttons["Allow While Using App"].waitForExistence(timeout: 10) {
+            springboard.buttons["Allow While Using App"].tap()
         }
         
         XCTAssert(app.staticTexts["Authorization Status: Authorized when in use"].waitForExistence(timeout: 10))
